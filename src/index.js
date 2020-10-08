f0b3cb5c1e3abb1edcce7b37e93928cf0aefe3c1
Andrey <tymoshenko.andrey@gmail.com>
2020-10-08 23:15:37 +0300

    init: start finding solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let config = new Map(bracketsConfig);
+    let stack = [];
+    for (let i of str.split("")) {
+        if (stack.length && i === config.get(stack[stack.length - 1])) {
+            stack.pop();
+        } else if (config.has(i)) {
+            stack.push(i);
+            // } else {
+            //     if (i !== config.get(stack.pop())) return false;
+        }
+    }
+    return stack.length === 0;
+};
